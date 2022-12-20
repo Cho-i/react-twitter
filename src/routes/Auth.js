a2be0d3926f56d00917e4de7724901e72c792b53
이초이 <choi1223@hunet.co.kr>
2022-12-20 16:36:31 +0900

    로그인

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { async } from "@firebase/util";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, GithubAuthProvider, signInWithPopup } from "firebase/auth";
+//import { async } from "@firebase/util";
 
 function Auth() {
 	const [email, setEmail] = useState('');
@@ -44,6 +44,24 @@ function Auth() {
 
 	const toggleAccount = () => setNewAccount((prev) => !prev);
 
+	const onSocialClick = async (e) => {
+		/**
+		 * https://firebase.google.com/docs/auth/web/google-signin?authuser=1
+		 * https://firebase.google.com/docs/auth/web/github-auth?authuser=1
+		 */
+		const { target: { name } } = e;
+		const auth = getAuth();
+		let provider;
+
+		if (name === "google") {
+			provider = new GoogleAuthProvider();
+		} else if (name === "github") {
+			provider = new GithubAuthProvider();
+		}
+
+		await signInWithPopup(auth, provider);
+	}
+
 	return (
 		<>
 			<div>
@@ -51,11 +69,14 @@ function Auth() {
 					<input type="text" name="email" placeholder="Email" required value={email} onChange={onChange} />
 					<input type="password" name="password" placeholder="Password" required value={password} onChange={onChange} />
 					<input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+					{error}
 				</form>
-				{error}
+				<span onClick={toggleAccount}>
+					{newAccount ? "Sign in" : "Create Account"}
+				</span>
 				<div>
-					<button>Google</button>
-					<button>Github</button>
+					<button name="google" onClick={onSocialClick}>Google</button>
+					<button name="github" onClick={onSocialClick}>Github</button>
 				</div>
 			</div>
 		</>
