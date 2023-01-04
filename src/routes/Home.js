be1350be04e0d316c31c543bf7dec36196504cd5
이초이 <choi1223@hunet.co.kr>
2023-01-04 15:22:45 +0900

    firestore

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { useState } from "react";
 import { collection, addDoc } from "firebase/firestore";
+import { dbService } from "../fbase";
 
 function Home() {
 	const [tweet, setTweet] = useState('');
-	const onSubmit = (e) => {
+	const onSubmit = async (e) => {
 		e.preventDefault();
 		/**
 		 * https://firebase.google.com/docs/firestore/manage-data/add-data?authuser=1#add_a_document
 		 */
+		await addDoc(collection(dbService, 'tweets'), {
+			tweet,
+			createdAt: Date.now()
+		});
+		setTweet('');
 	}
 	const onChange = (e) => {
 		const { target: { value } } = e;
